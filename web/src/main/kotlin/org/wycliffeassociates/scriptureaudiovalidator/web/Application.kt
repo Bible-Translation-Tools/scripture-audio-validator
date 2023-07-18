de0337bc67b479960be448a5fcd5adc4c188e851
anonymouswalker <hoanganhtran1998@gmail.com>
2023-07-18 10:57:27 -0400

    fix gradle issue

--- web/src/main/kotlin/org/wycliffeassociates/scriptureaudiovalidator/web/Application.kt
+++ web/src/main/kotlin/org/wycliffeassociates/scriptureaudiovalidator/web/Application.kt
@@ -1,11 +1,9 @@
 package org.wycliffeassociates.scriptureaudiovalidator.web
 
-import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.wycliffeassociates.scriptureaudiovalidator.web.route.index
 
@@ -24,5 +22,4 @@ fun main() {
     val server = embeddedServer(Netty, host = host, port = port, module = Application::module)
     println("Ktor server has started at http://$host:$port/")
     server.start(wait = true)
-
 }
--- web/src/main/kotlin/org/wycliffeassociates/scriptureaudiovalidator/web/route/Index.kt
+++ web/src/main/kotlin/org/wycliffeassociates/scriptureaudiovalidator/web/route/Index.kt
@@ -1,14 +1,9 @@
 package org.wycliffeassociates.scriptureaudiovalidator.web.route
 
 import io.ktor.http.*
-import io.ktor.http.content.*
 import io.ktor.server.application.*
-import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import org.wycliffeassociates.scriptureaudiovalidator.common.fileprocessor.FileProcessor
-import org.wycliffeassociates.scriptureaudiovalidator.common.usecases.FileProcessingRouter
-import java.io.File
 
 fun Routing.index() {
     route("/") {
