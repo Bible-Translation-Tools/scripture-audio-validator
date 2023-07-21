385f9676f75afe431c2991c248f9171b51aa6314
anonymouswalker <hoanganhtran1998@gmail.com>
2023-07-21 09:25:43 -0400

    refactor

--- web/src/main/kotlin/org/bibletranslationtools/scriptureaudiovalidator/web/route/Index.kt
+++ web/src/main/kotlin/org/bibletranslationtools/scriptureaudiovalidator/web/route/Index.kt
@@ -11,13 +11,16 @@ import io.ktor.server.routing.Routing
 import io.ktor.server.routing.route
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
-import org.bibletranslationtools.scriptureaudiovalidator.common.data.FileResult
+import org.bibletranslationtools.scriptureaudiovalidator.common.data.FileStatus
 import org.bibletranslationtools.scriptureaudiovalidator.common.data.SerializableFileResult
 import org.bibletranslationtools.scriptureaudiovalidator.common.usecases.FileProcessingRouter
+import org.slf4j.LoggerFactory
 import java.io.File
 import java.util.*
 
+private val logger = LoggerFactory.getLogger(Routing::index.javaClass)
 private val uploadDir = File(System.getenv("UPLOAD_DIR"))
+private val fileProcessor = FileProcessingRouter.build()
 
 fun Routing.index() {
     route("/") {
@@ -39,17 +42,12 @@ fun Routing.index() {
                         val file = dir.resolve(fileName)
                         filePart.streamProvider().use { input ->
                             file.outputStream().buffered().use { output ->
-                                input.copyTo(output)
+                                input.copyTo(output) // saves the uploaded file stream
                             }
                         }
 
-                        val fileProcessor = FileProcessingRouter.build()
-                        val processedFiles = fileProcessor.handleFiles(listOf(file))
-                        filesReport.addAll(
-                            processedFiles.map { result ->
-                                SerializableFileResult(result) // hide internal storage path
-                            }
-                        )
+                        val processedFiles = processFile(file)
+                        filesReport.addAll(processedFiles)
                     }
                     else -> {
                         filePart.dispose() // Dispose of the part if it's not a file
@@ -60,5 +58,23 @@ fun Routing.index() {
             call.respond(filesReport)
             dir.deleteRecursively()
         }
+    }
+}
+
+/**
+ * Processes the uploaded file individually to avoid having an error
+ * blocking the entire batch.
+ */
+private fun processFile(file: File): List<SerializableFileResult> {
+    return try {
+        fileProcessor.handleFiles(listOf(file))
+            .map { result ->
+                SerializableFileResult(result) // hide internal storage path
+            }
+    } catch (e: Exception) {
+        logger.error("An error occurred while processing $file.", e)
+        listOf(
+            SerializableFileResult(FileStatus.REJECTED, file.name, "Error")
+        )
     }
 }
